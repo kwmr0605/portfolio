bb33c5a8f9e7bf73a6a636fec47f59eb1ec89a30
y.kawamura <mstkzr0605@yahoo.co.jp>
2021-01-14 23:06:31 +0900

    skillListの更新

--- assets/data/skillList.js
+++ assets/data/skillList.js
@@ -2,9 +2,11 @@ export default [
   {
     category: 'フロントエンド',
     data: [
-      { name: 'HTML', value: 9 },
+      { name: 'HTML', value: 8 },
       { name: 'CSS', value: 8 },
       { name: 'SCSS', value: 7 },
+      { name: 'Bootstrap', value: 5 },
+      { name: 'Bulma', value: 6 },
       { name: 'JavaScript', value: 8 },
       { name: 'jQuery', value: 6 },
       { name: 'TypeScript', value: 2 },
@@ -15,7 +17,7 @@ export default [
   {
     category: 'バックエンド',
     data: [
-      { name: 'Java', value: 6 },
+      { name: 'Java', value: 7 },
       { name: 'Node.js', value: 3 },
     ],
   },
@@ -31,6 +33,9 @@ export default [
     category: 'その他',
     data: [
       { name: 'Git', value: 8 },
+      { name: 'Solr', value: 4 },
+      { name: 'MySQL', value: 6 },
+      { name: 'Selenium', value: 5 },
       { name: 'Wordpress', value: 1 },
       { name: 'Netlify', value: 6 },
       { name: 'microCMS', value: 8 },
